5b058e2d3b47efac5f17282694cdeb907de0579c
Rastogi01 <rastogishubh319@gmail.com>
2022-06-23 15:21:29 +0530

    New Commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,12 @@ import resumeSvg from "../../assets/resume.svg";
 
 import styles from "./Header.module.css";
 
+
+
+
 function Header() {
   return (
+    
     <div className={styles.container}>
       <div className={styles.left}>
         <p className={styles.heading}>
